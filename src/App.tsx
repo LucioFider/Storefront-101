662780c33934d807c46e0681f8a9dd6e9f7e8247
LucioFider <fidelislucio404@gmail.com>
2023-03-29 17:43:15 +1000

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import TopNav from "./components/Navbar";
+import ProductCardVertical from "./components/ProductCard";
+
+function App() {
+  return (
+    <div className="App">
+      <TopNav />
+      <ProductCardVertical />
+    </div>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+// tailwind.config.cjs
+const { tailwindConfig } = require("@storefront-ui/react/tailwind-config");
+
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  presets: [tailwindConfig],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@storefront-ui/react/**/*.{js,mjs}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
